f9ffdf16ffbe64f51aacc1133a707f0b803125b9
J Melg <rjmzelaya@gmail.com>
2025-10-09 17:35:21 +0200

    Cambios en la estructura

--- src/data/projectinfo.js
+++ src/data/projectinfo.js
@@ -2,42 +2,42 @@
 export const projectsInfo = [
     {
         "name": "Pax Aurora",
-        "image": "/img/projects/pax-aurora.png",
+        "image": "./img/projects/pax-aurora.png",
         "link": "https://starwars.fandom.com/wiki/Pax_Aurora"
     },
     {
         "name": "Star Forge",
-        "image": "/img/projects/star-forge.png",
+        "image": "./img/projects/star-forge.png",
         "link": "https://starwars.fandom.com/wiki/Star_Forge"
     },
     {
         "name": "Proyecto Energético de Ghorman",
-        "image": "/img/projects/ghorman-spiders.png",
+        "image": "./img/projects/ghorman-spiders.png",
         "link": "https://starwars.fandom.com/wiki/Ghorman"
     },
     {
         "name": "Stardust",
-        "image": "/img/projects/stardust.png",
+        "image": "./img/projects/stardust.png",
         "link": "https://starwars.fandom.com/wiki/Project_Stardust"
     },
     {
         "name": "TIE Defender",
-        "image": "/img/projects/tie-defender.png",
+        "image": "./img/projects/tie-defender.png",
         "link": "https://starwars.fandom.com/wiki/TIE/d_%22Defender%22_Multi-Role_Starfighter"
     }, 
     {   
         "name": "Dark Saber",
-        "image": "/img/projects/dark-saber.png",
+        "image": "./img/projects/dark-saber.png",
         "link": "https://starwars.fandom.com/wiki/Darksaber"
     },
     {   
         "name": "Chimera",
-        "image": "/img/projects/chimera.png",
+        "image": "./img/projects/chimera.png",
         "link": "https://starwars.fandom.com/wiki/Chimaera"
     }, 
     {
         "name": "Dark Trooper",
-        "image": "/img/projects/dark-trooper.png",
+        "image": "./img/projects/dark-trooper.png",
         "link": "https://starwars.fandom.com/wiki/Dark_Trooper"
     }
 ];
